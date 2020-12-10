69f32976f2e91019cc600dd6083b4ac930ce7b76
tomomi-makino <makinotomomi@cornerprinting.com>
2020-12-10 13:11:03 +0900

    画像アップロード先変更

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -3,9 +3,10 @@ class ImageUploader < CarrierWave::Uploader::Base
   # include CarrierWave::RMagick
   include CarrierWave::MiniMagick
 
+  process resize_to_fit: [300, 300]
   # Choose what kind of storage to use for this uploader:
-  storage :file
-  # storage :fog
+  # storage :file
+  storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
@@ -45,11 +46,11 @@ class ImageUploader < CarrierWave::Uploader::Base
   #   "something.jpg" if original_filename
   # end
   
-  process resize_to_fit: [300, 300]
+  
 
   # # Choose what kind of storage to use for this uploader:
   # # storage :file
-  # # storage :fog
+  # storage :fog
 
   # # Override the directory where uploaded files will be stored.
   # # # This is a sensible default for uploaders that are meant to be mounted:
