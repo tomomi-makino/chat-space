4aa0b687f3c2cc510215f16c011f96069f6c0ffe
tomomi-makino <makinotomomi@cornerprinting.com>
2020-12-13 15:46:46 +0900

    message.js指定DOM修正

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -74,8 +74,8 @@ $(function(){
         $.each(messages, function(i, message) {
           insertHTML += buildHTML(message)
         });
-        $('.message-group').append(insertHTML);
-        $('.message-group').animate({ scrollTop: $('.message-group')[0].scrollHeight});
+        $('.main-chat__message').append(insertHTML);
+        $('.main-chat__message').animate({ scrollTop: $('.main-chat__message')[0].scrollHeight});
       }     
     })
     .fail(function() {
