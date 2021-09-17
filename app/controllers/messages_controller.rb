a198e6867cbb05bd2e356cca54a7f0dc517d4865
tomomi-makino <makinotomomi@cornerprinting.com>
2021-09-17 11:14:35 +0900

    Revert "messages#createにlogger.debug追加"
    This reverts commit dd5a12deb06e65f66cc299711910814ee5ce06e3.

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -10,7 +10,6 @@ class MessagesController < ApplicationController
 
   def create
     @message = @group.messages.new(message_params)
-    logger.debug @product.errors.inspect
     if @message.save
       respond_to do |format|
         format.html {redirect_to group_messages_path(@group), notice: 'メッセージが送信されました'}
